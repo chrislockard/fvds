9534a00949ae8acfc417a317cf8f22d5ae1c4376
Chris <unl0ckd@protonmail.ch>
2018-06-11 14:47:45 -0400

    Adding preliminary linux environment detection

--- fvds.sh
+++ fvds.sh
@@ -1,9 +1,12 @@
-echo "Cleaning APT packages"
-apt clean
-sleep 2
-echo "Done cleaning APT packages"
-echo "Performing TRIM operations"
-for mount in / /boot /home /swap; do
-	fstrim $mount;
-done
-echo "All done!"
+#!/bin/bash
+if [ "$(grep -Ei 'debian|buntu|mint' /etc/*release)" ]; then
+	echo "Cleaning APT packages"
+	apt clean
+	sleep 2
+	echo "Done cleaning APT packages"
+	echo "Performing TRIM operations"
+	for mount in / /boot /home /swap; do
+		fstrim $mount;
+	done
+	echo "All done!"
+fi
